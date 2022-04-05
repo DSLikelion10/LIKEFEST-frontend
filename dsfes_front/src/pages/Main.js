d992410d90b5978ee6774fa06d1bf12394ec066c
soosoo030 <whereru511@gmail.com>
2022-04-05 21:24:41 +0900

    큰 틀 구현

---
+++ dsfes_front/src/pages/Main.js
@@ -0,0 +1,8 @@
+const Main = () => {
+  return (
+    <div>
+      메인 페이지
+    </div>
+  );
+}
+export default Main;
---
+++ dsfes_front/src/pages/TimeTable.js
@@ -0,0 +1,8 @@
+const TimeTable = () => {
+  return (
+    <div>
+      타임테이블 페이지
+    </div>
+  )
+}
+export default TimeTable;
