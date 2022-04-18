920e25f2607cefb13ba7103b5cdd1ef670d1020c
Chanran33 <k55416325@gmail.com>
2022-04-19 02:56:10 +0900

    style: 게시판 ui 수정

--- dsfes_front/src/components/BoardItem.js
+++ dsfes_front/src/components/BoardItem.js
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "../css/Board.module.css";
 
 const BoardItem = ({ text }) => {
-  const palette = ["#21E233", "#FFCC4A", "#3AB5FA", "#FD6060"];
+  const palette = ["#4C966E", "#C4DAE7", "#C3E196", "#D0C7DE", "#E7D0B6"];
 
-  const randomNum = Math.floor(Math.random() * 4);
+  const randomNum = Math.floor(Math.random() * 5);
 
   return (
     <div
