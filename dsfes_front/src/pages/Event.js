c1230dfb17a1b5bff89b48d6f63d213fa4001bc5
allzeroyou <ekdudd119@naver.com>
2022-05-04 00:45:11 +0900

    Event page - input box issue fix!

--- dsfes_front/src/pages/Event.js
+++ dsfes_front/src/pages/Event.js
@@ -24,7 +24,7 @@ const Event = () => {
     leave: { opacity: 0 },
   });
 
-  const [userinput, setUserinput] = useState('');
+  const [userinput, setUserinput] = useState("");
 
   const handleChange = useCallback((e) => {
     const {
@@ -35,29 +35,35 @@ const Event = () => {
       setUserinput(value);
     }
   }, []);
-  
-  const [choose,setChoose] = useState(false);
 
+  const [choose, setChoose] = useState(false);
 
-  const checkAnswer = ()=>{
-    const answer = "멋쟁이 사자처럼"
+  // 총학에서 정답을 받으면 아래 answer에 대입할 것
+  const checkAnswer = () => {
+    const answer = "멋쟁이 사자처럼";
 
-    if(answer === userinput){
+    if (answer === userinput) {
       console.log("정답이지롱");
       setChoose(true);
-    }else{
+    } else {
       console.log("오답이지롱");
       //setChoose(false);
       setChoose(false);
     }
-  }
+  };
 
   return (
-// event 페이지의 body 역할인 큰 틀의 div
+    // event 페이지의 body 역할인 큰 틀의 div
     <div className={styles.wrap}>
-  {/* 정답 입력 input과 button을 묶은 div */}
-      <div>
-        <input name="userinput" onChange={handleChange} className={styles.eventinput} type="text" placeholder=" 정답을 입력해주세요" />
+      {/* 정답 입력 input과 button을 묶은 div */}
+      <div className={styles.topbox}>
+        <input
+          name="userinput"
+          onChange={handleChange}
+          className={styles.eventinput}
+          type="text"
+          placeholder=" 정답을 입력해주세요"
+        />
         <button
           className={styles.button}
           type="submit"
@@ -65,7 +71,9 @@ const Event = () => {
             setOpenModal(true);
             checkAnswer();
           }}
-        >확인하기</button>
+        >
+          확인하기
+        </button>
       </div>
 
       {/* 참여 방법 */}
@@ -76,7 +84,7 @@ const Event = () => {
         {/* step 01 박스 */}
         <p className={styles.stepbox}>STEP&nbsp;01</p>
         {/* 이미지 */}
-        <img className={styles.stepimg} src={step1} alt="사진1"/>
+        <img className={styles.stepimg} src={step1} alt="사진1" />
         {/* 설명글 */}
         <p className={styles.steptext_1}>
           학교 곳곳에 있는 새로워진 덕새를 찾아주세요.
@@ -88,11 +96,9 @@ const Event = () => {
         {/* step 02 박스 */}
         <p className={styles.stepbox}>STEP&nbsp;02</p>
         {/* 이미지 */}
-        <img className={styles.stepimg} src={step2} alt="사진2"/>
+        <img className={styles.stepimg} src={step2} alt="사진2" />
         {/* 설명글 */}
-        <p className={styles.steptext}>
-          덕새 손에 있는 QR코드를 찍어 주세요.
-        </p>
+        <p className={styles.steptext}>덕새 손에 있는 QR코드를 찍어 주세요.</p>
       </div>
 
       {/* 참여 방법 step 3 */}
@@ -100,7 +106,7 @@ const Event = () => {
         {/* step 03 박스 */}
         <p className={styles.stepbox}>STEP&nbsp;03</p>
         {/* 이미지 */}
-        <img className={styles.stepimg} src={step3} alt="사진3"/>
+        <img className={styles.stepimg} src={step3} alt="사진3" />
         {/* 설명글 */}
         <p className={styles.steptext}>
           5개의 QR코드 속 이미지를 조합하여 문장을 만들어 주세요.
@@ -112,7 +118,7 @@ const Event = () => {
         {/* step 04 박스 */}
         <p className={styles.stepbox}>STEP&nbsp;04</p>
         {/* 이미지 */}
-        <img className={styles.stepimg} src={step4} alt="사진4"/>
+        <img className={styles.stepimg} src={step4} alt="사진4" />
         {/* 설명글 */}
         <p className={styles.steptext}>
           문장이 완성되었으면 입력칸에 문장을 입력해주세요.
@@ -124,7 +130,7 @@ const Event = () => {
         {/* step 05 박스 */}
         <p className={styles.stepbox}>STEP&nbsp;05</p>
         {/* 이미지 */}
-        <img className={styles.stepimg} src={step5} alt="사진5"/>
+        <img className={styles.stepimg} src={step5} alt="사진5" />
         {/* 설명글 */}
         <p className={styles.steptext}>
           정답인 경우, 팝업창을 캡처하여 중앙본부로 방문해 주세요!
@@ -135,9 +141,15 @@ const Event = () => {
 
       {transitions(
         (style, item) =>
-          item && ( choose ? <SuccessModal style={style} closeModal={() => setOpenModal(false)} /> :
-            <FailModal style={style} closeModal={() => setOpenModal(false)} /> 
-          )
+          item &&
+          (choose ? (
+            <SuccessModal
+              style={style}
+              closeModal={() => setOpenModal(false)}
+            />
+          ) : (
+            <FailModal style={style} closeModal={() => setOpenModal(false)} />
+          ))
       )}
     </div>
   );
