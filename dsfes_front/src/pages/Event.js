1f76b3eb44127fdd0338c3db7b0f389807741f07
dy <ekdudd119@naver.com>
2022-05-06 04:33:38 +0900

    Event page margin modify and scrolling

--- dsfes_front/src/pages/Event.js
+++ dsfes_front/src/pages/Event.js
@@ -15,7 +15,6 @@ import FailModal from "../components/FailModal";
 import { useTransition } from "react-spring";
 
 // 정답, 오답시 모달창 띄우기
-
 const Event = () => {
   const [openModal, setOpenModal] = useState(false);
 
@@ -78,81 +77,96 @@ const Event = () => {
         </button>
       </div>
 
-      {/* 참여 방법 */}
-      <p className={styles.jointext}>참여&nbsp;방법</p>
+      <div className={styles.bottombox}>
+        {/* 참여 방법 */}
+        <p className={styles.jointext}>참여&nbsp;방법</p>
 
-      {/* 참여 방법 step 1 */}
-      <div className={styles.container}>
-        {/* step 01 박스 */}
-        <p className={styles.stepbox}>STEP&nbsp;01</p>
-        {/* 이미지 */}
-        <img className={styles.stepimg} src={step1} alt="사진1" />
-        {/* 설명글 */}
-        <p className={styles.steptext_1}>
-          학교 곳곳에 있는 새로워진 덕새를 찾아주세요.
-        </p>
+        {/* 참여 방법 step 1 */}
+        <div className={styles.container}>
+          {/* step 01 박스 */}
+          <p className={styles.stepbox}>STEP&nbsp;01</p>
+          {/* 이미지 */}
+          <img className={styles.stepimg} src={step1} alt="사진1" />
+          {/* 설명글 */}
+          <div className={styles.text}>
+            <p className={styles.steptext_1}>
+              학교 곳곳에 있는 새로워진 덕새를 찾아주세요.
+            </p>
+          </div>
+
+        </div>
+
+        {/* 참여 방법 step 2 */}
+        <div className={styles.container}>
+          {/* step 02 박스 */}
+          <p className={styles.stepbox}>STEP&nbsp;02</p>
+          {/* 이미지 */}
+          <img className={styles.stepimg} src={step2} alt="사진2" />
+          {/* 설명글 */}
+          <div className={styles.text}>
+          <p className={styles.steptext}>덕새 손에 있는 QR코드를 찍어 주세요.</p>
+          </div>
+        </div>
+
+        {/* 참여 방법 step 3 */}
+        <div className={styles.containerbig}>
+          {/* step 03 박스 */}
+          <p className={styles.stepbox}>STEP&nbsp;03</p>
+          {/* 이미지 */}
+          <img className={styles.stepimg} src={step3} alt="사진3" />
+          {/* 설명글 */}
+          <div className={styles.textlast}>
+          <p className={styles.steptext}>
+            5개의 QR코드 속 이미지를 조합하여 문장을 만들어 주세요.
+          </p>
+          </div>
+        </div>
+
+        {/* 참여 방법 step 4 */}
+        <div className={styles.containerbig}>
+          {/* step 04 박스 */}
+          <p className={styles.stepbox}>STEP&nbsp;04</p>
+          {/* 이미지 */}
+          <img className={styles.stepimg} src={step4} alt="사진4" />
+          {/* 설명글 */}
+          <div className={styles.textlast}>
+          <p className={styles.steptext}>
+            문장이 완성되었으면 입력칸에 문장을 입력해주세요.
+          </p>
+          </div>
+        </div>
+
+        {/* 참여 방법 step 5 */}
+        <div className={styles.containerbig}>
+          {/* step 05 박스 */}
+          <p className={styles.stepbox}>STEP&nbsp;05</p>
+          {/* 이미지 */}
+          <img className={styles.stepimg} src={step5} alt="사진5" />
+          {/* 설명글 */}
+          <div className={styles.textlast}>
+          <p className={styles.steptext}>
+            정답인 경우, 팝업창을 캡처하여 중앙본부로 방문해 주세요!
+          </p>
+          </div>
+        </div>
       </div>
+   
 
-      {/* 참여 방법 step 2 */}
-      <div className={styles.container}>
-        {/* step 02 박스 */}
-        <p className={styles.stepbox}>STEP&nbsp;02</p>
-        {/* 이미지 */}
-        <img className={styles.stepimg} src={step2} alt="사진2" />
-        {/* 설명글 */}
-        <p className={styles.steptext}>덕새 손에 있는 QR코드를 찍어 주세요.</p>
-      </div>
 
-      {/* 참여 방법 step 3 */}
-      <div className={styles.containerbig}>
-        {/* step 03 박스 */}
-        <p className={styles.stepbox}>STEP&nbsp;03</p>
-        {/* 이미지 */}
-        <img className={styles.stepimg} src={step3} alt="사진3" />
-        {/* 설명글 */}
-        <p className={styles.steptext}>
-          5개의 QR코드 속 이미지를 조합하여 문장을 만들어 주세요.
-        </p>
-      </div>
+        {/* 삼항 연산자로 만든 다음, false에서 정답과 비교해서 유동적으로 만든 후 맞으면 success 모달이 뜨게끔, 틀리면 false 모달이 뜨게끔 */}
 
-      {/* 참여 방법 step 4 */}
-      <div className={styles.containerbig}>
-        {/* step 04 박스 */}
-        <p className={styles.stepbox}>STEP&nbsp;04</p>
-        {/* 이미지 */}
-        <img className={styles.stepimg} src={step4} alt="사진4" />
-        {/* 설명글 */}
-        <p className={styles.steptext}>
-          문장이 완성되었으면 입력칸에 문장을 입력해주세요.
-        </p>
-      </div>
-
-      {/* 참여 방법 step 5 */}
-      <div className={styles.containerbig}>
-        {/* step 05 박스 */}
-        <p className={styles.stepbox}>STEP&nbsp;05</p>
-        {/* 이미지 */}
-        <img className={styles.stepimg} src={step5} alt="사진5" />
-        {/* 설명글 */}
-        <p className={styles.steptext}>
-          정답인 경우, 팝업창을 캡처하여 중앙본부로 방문해 주세요!
-        </p>
-      </div>
-
-      {/* 삼항 연산자로 만든 다음, false에서 정답과 비교해서 유동적으로 만든 후 맞으면 success 모달이 뜨게끔, 틀리면 false 모달이 뜨게끔 */}
-
-      {transitions(
-        (style, item) =>
-          item &&
-          (choose ? (
-            <SuccessModal
-              style={style}
-              closeModal={() => setOpenModal(false)}
-            />
-          ) : (
-            <FailModal style={style} closeModal={() => setOpenModal(false)} />
-          ))
-      )}
+        {transitions(
+          (style, item) =>
+            item &&
+            (choose ? (
+              <SuccessModal
+                style={style}
+                closeModal={() => setOpenModal(false)}
+              />
+            ) : (
+              <FailModal style={style} closeModal={() => setOpenModal(false)} />
+            ))
+        )}
     </div>
   );
 };
