818359904154d0ee1ac2071f47279b83cc5edcc2
dy <ekdudd119@naver.com>
2022-05-08 23:41:13 +0900

    Modal scroll modify

--- dsfes_front/src/pages/Event.js
+++ dsfes_front/src/pages/Event.js
@@ -66,7 +66,6 @@ const Event = () => {
           className={styles.eventinput}
           type="text"
           placeholder=" 정답을 입력해주세요"
-          // onkeyup="enterkey();" enter클릭시 버튼 눌리는 효과를 내고 싶다
         />
         <button
           className={styles.button}
